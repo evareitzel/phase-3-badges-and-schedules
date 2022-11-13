f4057ae7a8b357cc0a63c790fdebf82655444c0f
Eva Reitzel <evareitzel@gmail.com>
2022-11-12 19:13:20 -0500

    commit tree 38c58e0de6d0f7642ed08d7e5a3fdb8c58ac9467

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,49 @@
+require "pry"
+
 # Write your code here.
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+  badges = []
+  arr.map do |name|
+    badges <<   "Hello, my name is #{name}."
+    # binding.pry
+  end
+  badges
+end
+# batch_badge_creator ["eva", "nick"]
+
+def assign_rooms speakers
+  speakers.map.with_index(1) do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index}!"
+  end
+end
+
+def assign_rooms_2 speakers    
+  assignments = []
+    i = 1
+    for speaker in speakers do
+      assignments << "Hello, #{speaker}! You'll be assigned to room #{i}!"
+      # binding.pry
+      i += 1
+    end
+  assignments
+end
+# assign_rooms ["eva", "nick"]
+
+def printer speakers
+
+  batch_badge_creator(speakers).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(speakers).each do |room|
+    puts room
+    # binding.pry
+  end
+end
+
+# printer ["eva", "nick"]
